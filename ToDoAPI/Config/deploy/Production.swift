9228d3c0a686554aae5b39af952943f56acaa5e4
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-14 08:12:22 +0900

    add ssh key

--- ToDoAPI/Config/deploy/Production.swift
+++ ToDoAPI/Config/deploy/Production.swift
@@ -2,6 +2,9 @@ import Flock
 
 class Production: Configuration {
     func configure() {
+        if let key = ProcessInfo.processInfo.environment["FLOCK_KEY"] {
+            Config.SSHAuthMethod = .key(key)
+        }
         // using vagrant private network ip
         Servers.add(ip: "192.168.33.10", user: "vagrant", roles: [.app, .db, .web])
     }
