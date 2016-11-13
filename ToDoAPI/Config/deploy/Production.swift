ab5a1d42fa2fa5d2ba41fe1e4d7ddab20dc990e3
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-14 08:01:06 +0900

    modified flock configure file

--- ToDoAPI/Config/deploy/Production.swift
+++ ToDoAPI/Config/deploy/Production.swift
@@ -2,7 +2,7 @@ import Flock
 
 class Production: Configuration {
     func configure() {
-		// Config.SSHAuthMethod = .key("/path/to/mykey")
-		// Servers.add(ip: "9.9.9.9", user: "user", roles: [.app, .db, .web])
+        // using vagrant private network ip
+        Servers.add(ip: "192.168.33.10", user: "vagrant", roles: [.app, .db, .web])
     }
 }
