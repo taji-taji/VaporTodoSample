4bbfe26a5140c02f1ac9c801e545cdd906ae62e1
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-13 11:48:12 +0900

    add flock

---
+++ ToDoAPI/Config/deploy/Production.swift
@@ -0,0 +1,8 @@
+import Flock
+
+class Production: Configuration {
+    func configure() {
+		// Config.SSHAuthMethod = .key("/path/to/mykey")
+		// Servers.add(ip: "9.9.9.9", user: "user", roles: [.app, .db, .web])
+    }
+}
